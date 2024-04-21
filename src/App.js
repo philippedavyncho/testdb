1a8fbda6bb218d73f153ba25cf15e6eedd8c0a10
philippedavyncho <nchodavyphilippe@gmail.com>
2024-04-21 23:51:29 +0000

    nav section 5

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App(){
         <>
         <CartProvider>
         <ToastContainer />
-        <div className="App centerApp">
+        <div className="App">
             <div className="Anime">
                 <Part />
             </div>
